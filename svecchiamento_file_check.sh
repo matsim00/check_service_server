0f26cc3b7c088b1633f621eb9af99bd862e717e6
matsim00 <135755950+matsim00@users.noreply.github.com>
2024-08-19 16:20:40 +0200

    Create svecchiamento_file_check.sh

---
+++ svecchiamento_file_check.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SVECCHIAMENTO CHECK FILE
+
+#Specifica la retention in giorni(verranno cancellati i file di n° giorni prima)
+range_date="7"
+
+dir1="/nfs_share/output_monitoring/"
+
+find $dir1 -type f  -daystart -mtime +$range_date -exec rm -f "{}" \;
+
+
+
+##### AGGIUNGI SE NECESSARIO ULTERIORI VARIABILI CON IL PATH
